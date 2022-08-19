c6db9610019eeec995428417efa6c95a67b49111
FlutLab.io <>
2022-08-19 05:20:48 +0000

    Autosave

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,7 +1,6 @@
 //import 'dart:ffi';
 
 
-import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:timeapp/worker/worker.dart';
 import 'package:http/http.dart' as http;
@@ -29,7 +28,7 @@ class _LoadingState extends State<Loading> {
     Map data = jsonDecode(response.body);
 
     Map weatherdata = data['weather'];
-    Float ab = weatherdata['temp'];
+    Map ab = weatherdata['temp'];
 
     // Map desc = jsonDecode(weatherdata['weather']);
 
