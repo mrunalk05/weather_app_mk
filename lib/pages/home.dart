5e82766710ecf0cf7168c0bd9f759b60c80448c0
FlutLab.io <>
2022-08-19 18:29:27 +0000

    data fetched

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,24 +10,24 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     Map info = ModalRoute.of(context)?.settings.arguments as Map;
     return Scaffold(
-      backgroundColor: Colors.blue[50],
-      appBar: AppBar(
-        title: Text('Weather'),
-        centerTitle: true,
-        backgroundColor: Color(0xff012c4f),
-      ),
-      body: Padding(padding: EdgeInsets.all(2.0),
-      child: Text(info['temp_value']),)
-            
+        backgroundColor: Colors.blue[50],
+        appBar: AppBar(
+          title: Text('Weather'),
+          centerTitle: true,
+          backgroundColor: Color(0xff012c4f),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(2.0),
+          child: Text(info['temp_value']),
+        )
+
         // FlatButton.icon(
         //     onPressed: () {
         //       Navigator.pushNamed(context, '/location');
         //     },
         //     icon: Icon(Icons.edit_location),
         //     label: Text('location')),
-           
-      );
-       
-    
+
+        );
   }
 }
