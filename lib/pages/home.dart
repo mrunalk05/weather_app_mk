c7461fbe98e2052b6a805023e5dbaf0976df9a42
FlutLab.io <>
2022-08-19 18:23:10 +0000

    Autosave

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -22,7 +22,7 @@ class _HomeState extends State<Home> {
               Navigator.pushNamed(context, '/location');
             },
             icon: Icon(Icons.edit_location),
-            label: Text('mk')),
+            label: Text(info["temp"])),
       ]),
     );
   }
