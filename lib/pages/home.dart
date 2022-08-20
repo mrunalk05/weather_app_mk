723bdd3cb34fe0613103e7b1e8d6ea098d1222fc
FlutLab.io <>
2022-08-20 07:07:18 +0000

    Autosave

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,16 +13,46 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     Map info = ModalRoute.of(context)!.settings.arguments as Map;
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Home Activity"),
+
+      body: SafeArea(
+
+        child: Container(
+          child: Column(
+            children: [
+              Container( //Search Wala Container
+
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                margin: EdgeInsets.symmetric(horizontal: 24,vertical: 20),
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(24)
+                ),
+                child: Row(
+
+                  children: [
+                   GestureDetector(
+                     onTap: () {
+                       print("Search Me");
+                     },
+                     child: Container(child: Icon(Icons.search,color: Colors.blueAccent,),margin: EdgeInsets.fromLTRB(3, 0, 7, 0),),
+                   ),
+                    Expanded(
+                      child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search city"
+                      ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+
+            ],
+          ),
         ),
-        body:
-         
-            // Container(
-            FloatingActionButton(onPressed: () {}, child: Text(info["pre"])),
-            //FloatingActionButton(onPressed: () {}, child: Text(info["hum"])),
-          
-          
-        );
+      ),
+    );
   }
 }
+  
