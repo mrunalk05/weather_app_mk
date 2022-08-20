3528de39797a16cc5f3febfe9a01841030833df7
FlutLab.io <>
2022-08-20 05:42:23 +0000

    Autosave

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,6 +20,7 @@ class _HomeState extends State<Home> {
           children: <Widget>[
             // Container(
             FloatingActionButton(onPressed: () {}, child: Text(info["pre"])),
+            FloatingActionButton(onPressed: () {}, child: Text(info["hum"])),
           ],
         ));
   }
