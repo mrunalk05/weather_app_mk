41bc9920cde596485b10b9be2243f98fccc339c3
FlutLab.io <>
2022-08-20 07:30:11 +0000

    Autosave

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,42 +12,43 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     Map info = ModalRoute.of(context)!.settings.arguments as Map;
+    print((info['pre']).toString().substring(0,4));
     return Scaffold(
-
       body: SafeArea(
-
         child: Container(
           child: Column(
             children: [
-              Container( //Search Wala Container
+              Container(
+                //Search Wala Container
 
                 padding: EdgeInsets.symmetric(horizontal: 8),
-                margin: EdgeInsets.symmetric(horizontal: 24,vertical: 20),
+                margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
                 decoration: BoxDecoration(
-                  color: Colors.black12,
-                  borderRadius: BorderRadius.circular(24)
-                ),
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.circular(24)),
                 child: Row(
-
                   children: [
-                   GestureDetector(
-                     onTap: () {
-                       print("Search Me");
-                     },
-                     child: Container(child: Icon(Icons.search,color: Colors.blueAccent,),margin: EdgeInsets.fromLTRB(3, 0, 7, 0),),
-                   ),
+                    GestureDetector(
+                      onTap: () {
+                        print("Search Me");
+                      },
+                      child: Container(
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.blueAccent,
+                        ),
+                        margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                      ),
+                    ),
                     Expanded(
                       child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Search city"
-                      ),
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: "Search city"),
                       ),
                     )
                   ],
                 ),
               ),
-
             ],
           ),
         ),
@@ -55,4 +56,3 @@ class _HomeState extends State<Home> {
     );
   }
 }
-  
